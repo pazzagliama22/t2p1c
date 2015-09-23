73ee0d49fb09f5d11f6498aac6fde864bcc6e872
pazzagliama22 <pazzagliama22@mounties.mansfield.edu>
2015-09-23 16:57:00 -0400

    Exercise.java
    First java exercise

---
+++ Exercise.java
@@ -0,0 +1,95 @@
+/*
+ *Logging Exercise
+ */
+package Exercise;
+
+/**
+ *
+ * @author TEAM 2
+ */
+public class Exercise{
+    public static void main(String[] args) {
+        
+    }
+    //DEFINED FACTORS
+    private int Id;
+    private String MemberNumber;
+    private String exerciseType;
+    private String Date;
+    private double Time;
+    private String Comment;
+    
+        public Exercise() {
+        Id = 0;
+        MemberNumber = "";
+        exerciseType = "";
+        Date = "";
+        Time = 0;
+        Comment = "";
+    }
+
+    public Exercise(int Id, String MemberNumber, String exerciseType, String Date, Double Time, String Comment) {
+        this.Id = Id;
+        this.MemberNumber = MemberNumber;
+        this.exerciseType = exerciseType;
+        this.Date = Date;
+        this.Time = Time;
+        this.Comment = Comment;
+  //RETURNING THE FACTORS
+    }
+
+    public String getexcerciseType() {
+        return exerciseType;
+    }
+
+    public void setexcerciseType(String exerciseType) {
+        this.exerciseType = exerciseType;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getMemberNumber() {
+        return MemberNumber;
+    }
+
+    public void setMemberNumber(String MemberNumber) {
+        this.MemberNumber = MemberNumber;
+    }
+
+    public String Date() {
+        return Date;
+    }
+
+    public void set(String Date) {
+        this.Date = Date;
+    }
+
+    public double getTime() {
+        return Time;
+    }
+
+    public void setSalary(double Time) {
+        this.Time = Time;
+    }
+    
+    public String getComment() {
+        return Comment;
+}
+    
+    public void setComment(String Comment) {
+        this.Comment = Comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Exercise{" + "Id=" + Id + ", MemberNumber=" + MemberNumber 
+                + ", exerciseType=" + exerciseType + ", Date=" + Date 
+                + ", Time=" + Time + ", Comment=" + Comment +'}';
+    }
+}
